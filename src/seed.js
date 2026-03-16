a152ac0a2d4db40b9d77cdfee048e4ba4902e182
Lucas Silveira <lugano1995@gmail.com>
2026-03-16 18:54:25 -0300

    feat: increate seed company global score to 1000

--- src/seed.js
+++ src/seed.js
@@ -6,7 +6,7 @@ const db = createDb();
 const company = db.prepare(`
   INSERT OR IGNORE INTO companies (name, cnpj, global_score_goal)
   VALUES (?, ?, ?)
-`).run('EcoAl Corp', '12345678000100', 100);
+`).run('EcoAl Corp', '12345678000100', 1000);
 
 if (company.changes > 0) {
   console.log('Default company created (cnpj: 12345678000100)');
